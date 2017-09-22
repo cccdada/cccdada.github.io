bbf983f8b4705992eb66081506a8e23fcf4ad9d3
wangsheng <wangsheng@csii.com.cn>
2017-09-22 10:28:25 +0800

    1

--- gallary/js/data.js
+++ gallary/js/data.js
@@ -134,7 +134,8 @@ var d = dataStr.split("<br><br><br>");
 for(var i = 0 ;i< d.length;i++){
 	var c = d[i].split("<br><br>");
 	data.push({
-		img:"images/"+c[0].replace(".","") + ".jpg",
+		//img:c[0].replace(".","") + ".jpg",
+		img:c[0]+ ".jpg",
 		caption:c[0].split(".")[1],
 		desc :c[1]
 	});
